9ec648c26bef7916a6edeaafdd1ef937a2ddccb4
Tian Zhao, Ph. D. <83426612+tianzhaopupsu@users.noreply.github.com>
2026-05-27 12:26:29 -0400

    Create ingest.py

---
+++ RAG-Scientific-Assistant/ingest.py
@@ -0,0 +1,29 @@
+from app.ingestion.pdf_loader import load_pdf
+from app.ingestion.embedder import Embedder
+from app.ingestion.indexer import Indexer
+from app.qdrant_db import initialize_collection
+
+
+PDF_PATH = r"C:\Users\tz13\Downloads\FAQ_of_Quant_Finance.pdf"
+
+
+def main():
+
+    # 1. init DB
+    initialize_collection()
+
+    # 2. load PDF
+    pages = load_pdf(PDF_PATH)
+
+    # 3. embedder
+    embedder = Embedder()
+
+    # 4. indexer
+    indexer = Indexer(embedder)
+
+    # 5. ingest
+    indexer.index(pages)
+
+
+if __name__ == "__main__":
+    main()
